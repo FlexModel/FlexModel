28f3d0cf50564061707ec45c94ab98314d670b7d
Niels Nijens <nijens.niels@gmail.com>
2016-02-11 23:32:26 +0100

    Add skeleton testsuite

---
+++ tests/FlexModelTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FlexModel\Test;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * FlexModelTest.
+ *
+ * @author Niels Nijens <niels@connectholland.nl>
+ */
+class FlexModelTest extends PHPUnit_Framework_TestCase
+{
+
+}
